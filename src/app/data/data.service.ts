adf13bef3faec607fb8cdc1b89925df8f586b7dc
Jeffery Cosby <jcoz1968@gmail.com>
2019-04-06 22:20:19 -0500

    create data service

---
+++ src/app/data/data.service.ts
@@ -0,0 +1,14 @@
+import { UserSettings } from './user-settings';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  postUserSettingsForm(userSettings: UserSettings) {
+
+  }
+}
